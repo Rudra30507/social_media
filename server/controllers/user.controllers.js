e1a0b131f40fb6cef792415edd45b9808d314082
Rudra Patel <rudra.25bcs10488@sst.scaler.com>
2026-09-05 15:48:31 +0530

    Add user login controller and route with password validation

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -22,10 +22,12 @@ export const registerUser = async(req , res)=>{
     if(emailExists){
         return res.status(409).json({message:"Email already exists"})
     }
-
+    //salt is a random string that is added to the password before hashing
     const salt = await bcrypt.genSalt(10)
-
+    
     console.log(salt)
+
+    //hashing
     const hashedPassword = await bcrypt.hash(password , salt)
 
     console.log(hashedPassword)
@@ -38,11 +40,40 @@ export const registerUser = async(req , res)=>{
 
     return res.status(201).json({message: "User registered successfully" , user:newUser})
     
-
-
-
-
     }
+    catch(err){
+        res.status(500).json({message : 'server crashed' , error:err.message})
+    }
+}
+
+//login user 
+
+export const loginUser = async(req , res)=>{
+    try{
+        const {email , password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ message : "all feilds are required"})
+        }
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+
+        const PasswordValid = await bcrypt.compare(password , user.password)
+        
+        if(!PasswordValid){
+            return res.status(401).json({message:"Invalid password"})
+        }
+
+       res.status(200).json({message : "Login successful"})
+
+      
+       
+    }
+
     catch(err){
         res.status(500).json({message : 'server crashed' , error:err.message})
     }
